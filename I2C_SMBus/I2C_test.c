24b24fff5713a1d416aee1d2668905eb5588bafb
Bruno Cayres Messias <brunocmessias@hotmail.com>
2020-08-14 13:36:01 -0300

    adding 12c test code

---
+++ I2C_SMBus/I2C_test.c
@@ -0,0 +1,31 @@
+#include "I2C_SMbus.h"
+
+const int addr = 0x8; //! change for the addr show in  'sudo i2cdetect -y 1' in terminal
+const int busnum = 1; //* For I2C port 1 ons rp1
+
+__u8 reg = 0x10; //* Device register to access 
+int value;
+
+void main(){
+
+    rpiI2cSetup(addr,busnum); //Set up the I2C Device
+
+    int numb = 1; //control for i2c bus
+    
+    printf("Enter 1 for ON or 0 for OFF");
+    while (numb==1)
+    {
+        scanf("%i", value);
+        if (value == 1)
+        {
+            rpiI2cWrite(reg, 1);
+        }
+		elseif (value == 0)
+        {
+            rpiI2cWrite(reg, 1);
+        }
+		else numb = 0;
+    }
+    
+    return 0;
+};
